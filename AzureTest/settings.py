8db7bf363158087f66b6acddf9b50d934647518e
Benjamin Thiry <benjamin.thiry@qbere.com>
2023-04-07 11:23:43 +0200

    put on production

--- AzureTest/settings.py
+++ AzureTest/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-527_vk)70c*uwgi)01ecx9ldm%((saubovmbtcke0(13z4*x9n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

b115f5adab2e1a6a9eaf27ca321d61f615238227
Benjamin Thiry <benjamin.thiry@qbere.com>
2023-04-07 15:20:06 +0200

    Use of models - beginning

--- library/models.py
+++ library/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.db.models import IntegerField, Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+class Claim(models.Model):
+    INCURRED = "Incurred"
+    PAID = "Paid"
+    OUTSTANDING = "Outstanding"
+    TYPE_OF_CLAIM_CHOICE = [
+        (INCURRED, "Incurred"),
+        (PAID, "Paid"),
+        (OUTSTANDING, "Outstanding"),
+    ]
+    claim_number = models.FileField(upload_to='library')
+    year = models.IntegerField(
+        default= 2000,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(1990),
+        ]
+    )
+    type  = models.CharField(
+        max_length= 12,
+        choices=TYPE_OF_CLAIM_CHOICE,
+        default=INCURRED
+    )
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.number
